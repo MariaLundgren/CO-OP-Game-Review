19a9ffb23aebf873b37cadaabd33d9656683c144
MariaLundgren <melundgren93@gmail.com>
2021-06-10 17:22:54 +0000

    removed unused path

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-from os import path
 if os.path.exists("env.py"):
     import env
 
