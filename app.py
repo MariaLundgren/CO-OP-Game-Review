0dece4de179d138c331f1e424e838916bb3c7a30
MariaLundgren <melundgren93@gmail.com>
2021-06-11 15:57:40 +0000

    added flash messages on redirects on edit_game_title

--- app.py
+++ app.py
@@ -267,8 +267,11 @@ def edit_game_title(title_id):
                     flash("Title Updated")
                     return redirect(url_for("profile"))
             else:
+                flash(
+                    "You don't have access to the page you tying to visit")
                 return redirect(url_for("get_titles"))
         else:
+            flash("The title you are trying to update don't exist")
             return redirect(url_for("get_titles"))
     else:
         return redirect(url_for("log_in"))
