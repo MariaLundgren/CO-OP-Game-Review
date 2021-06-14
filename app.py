62c69e7ae3ddb3add981161ccac773dfc6432a43
MariaLundgren <melundgren93@gmail.com>
2021-06-14 18:12:17 +0000

    changed 404 and 500 error handlers

--- app.py
+++ app.py
@@ -373,16 +373,16 @@ def page_not_found(error):
     Page not found error handler.
     Returns: Renders 404 page.
     '''
-    return render_template("404.html"), 404
+    return render_template("404.html", error=error)
 
 
-@app.errorhandler(400)
+@app.errorhandler(500)
 def internal_server_error(error):
     '''
     Internal server error handler.
     Returns: Renders 500 page.
     '''
-    return render_template("500.html"), 500
+    return render_template("500.html", error=error)
 
 
 if __name__ == "__main__":
