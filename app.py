2dfeb5b501a9db56d73b2f12b070eb8293584f9c
MariaLundgren <melundgren93@gmail.com>
2021-06-15 07:44:08 +0000

    added perfered url scheme in app.py

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config.update(dict(
+    PREFERRED_URL_SCHEME='https'
+))
+
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
